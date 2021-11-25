5c886b525f09856729e46646bf3302a79e3c0971
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-11-25 23:15:02 +0530

    Update index.ts

--- tinting/src/index.ts
+++ tinting/src/index.ts
@@ -1,14 +1,16 @@
-import {Application,Sprite} from 'pixi.js'
+import {Application,Sprite,Rectangle} from 'pixi.js';
 const app=new Application()
 
 document.body.appendChild(app.view)
 
 const b:Sprite[]=[]
 
-const total=20
-app.loader.add('a','assets/download.png').load((l,r)=>{
+const total=90
+app.loader.add('a','assets/d1.PNG').load((l,r)=>{
 for(let i=0;i<total;i++){
         const ba=new Sprite(r['a'].texture)
+        ba.width=1
+        ba.height=1
         ba.anchor.set(0.5)
         ba.scale.set(0.8+Math.random()*0.3)
 
@@ -18,10 +20,34 @@ for(let i=0;i<total;i++){
         ba.tint=Math.random()*0xFFFFFF;
 
         (<any>ba).direction=Math.random()*Math.PI*2;
-        (<any>ba).turingSpeed=Math.random()-0.8;
+        (<any>ba).turningSpeed=Math.random()-0.8;
         (<any>ba).speed=2+Math.random()*2;
         b.push(ba)
 
         app.stage.addChild(ba)
+
     }
+    const dudeP=100;
+    const dudeB=new Rectangle(-dudeP,-dudeP,app.screen.width+dudeP*2,app.screen.height+dudeP*2)
+
+    app.ticker.add(()=>{
+        for(let i=0;i<b.length;i++){
+            const dude=b[i];
+            (<any>dude).direction+=(<any>dude).turningSpeed*0.01;
+            dude.x+=Math.sin((<any>dude).direction)*(<any>dude).speed;
+            dude.y+=Math.cos((<any>dude).direction)*(<any>dude).speed;
+            dude.rotation=-(<any>dude).direction-Math.PI/2;
+
+            if(dude.x<dudeB.x){
+                dude.x+=dudeB.width;
+            }else if(dude.x>dudeB.width){
+                dude.x-=dudeB.width;
+            }
+            if(dude.y<dudeB.y){
+                dude.y+=dudeB.height;
+            }else if(dude.y>dudeB.y+dudeB.height){
+                dude.y-=dudeB.height;
+            }
+        }
+    })
 })
